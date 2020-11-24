7b3b45b945b8f3950b289ff80fc7a58cff05f6b5
Dario Bruzzese <dariobrux@gmail.com>
2020-11-24 21:30:29 +0100

    Add Koin dependency for viewmodel

--- app/src/main/java/com/dariobrux/pokemon/ui/main/MainFragment.kt
+++ app/src/main/java/com/dariobrux/pokemon/ui/main/MainFragment.kt
@@ -1,6 +1,10 @@
 package com.dariobrux.pokemon.ui.main
 
 import androidx.fragment.app.Fragment
+import com.dariobrux.pokemon.ui.splash.SplashViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainFragment : Fragment() {
+
+    private val viewModel: MainViewModel by viewModel()
 }
--- app/src/main/java/com/dariobrux/pokemon/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/dariobrux/pokemon/ui/splash/SplashFragment.kt
@@ -1,5 +1,9 @@
 package com.dariobrux.pokemon.ui.splash
 
 import androidx.fragment.app.Fragment
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class SplashFragment : Fragment()
+class SplashFragment : Fragment() {
+
+    private val viewModel: SplashViewModel by viewModel()
+}
