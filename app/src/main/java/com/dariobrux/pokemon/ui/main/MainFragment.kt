b9396f246e3d2e5fcb8501e16649a66df76aadbc
Dario Bruzzese <dariobrux@gmail.com>
2020-11-24 21:41:30 +0100

    Add Navigation Component

--- app/src/main/java/com/dariobrux/pokemon/ui/main/MainFragment.kt
+++ app/src/main/java/com/dariobrux/pokemon/ui/main/MainFragment.kt
@@ -1,10 +1,19 @@
 package com.dariobrux.pokemon.ui.main
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.dariobrux.pokemon.R
 import com.dariobrux.pokemon.ui.splash.SplashViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainFragment : Fragment() {
 
     private val viewModel: MainViewModel by viewModel()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
 }
