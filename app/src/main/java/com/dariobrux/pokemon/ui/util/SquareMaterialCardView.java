4255fd32ad6ebef528344ed61cca3e6cd25de8e1
Dario Bruzzese <dariobrux@gmail.com>
2020-11-29 14:31:40 +0100

    Square card

---
+++ app/src/main/java/com/dariobrux/pokemon/ui/util/SquareMaterialCardView.java
@@ -0,0 +1,27 @@
+package com.dariobrux.pokemon.ui.util;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.google.android.material.card.MaterialCardView;
+
+public class SquareMaterialCardView extends MaterialCardView {
+
+    public SquareMaterialCardView(Context context) {
+        super(context);
+    }
+
+    public SquareMaterialCardView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareMaterialCardView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @SuppressWarnings("SuspiciousNameCombination")
+    @Override
+    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec);
+    }
+}
